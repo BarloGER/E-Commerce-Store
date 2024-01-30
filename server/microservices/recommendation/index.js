b45acc5e699dbff6cad246ddde75befafb0feb50
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-01-30 13:27:40 +0100

    Microservices: Added neccessary microservices with test routes

---
+++ server/microservices/recommendation/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 8088;
+
+app.use(cors({ origin: "*" }));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.send("Recommendation Service: GET-Anfrage empfangen");
+});
+
+app.post("/", (req, res) => {
+  res.send("Recommendation Service: POST-Anfrage empfangen");
+});
+
+app.use("*", (req, res) => res.sendStatus(404));
+
+app.listen(PORT, () => {
+  console.log(`Service running on port ${PORT}`);
+});
