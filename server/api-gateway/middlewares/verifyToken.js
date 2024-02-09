a572bc9d1e61328e6e17b689862959fcac53df76
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-02-09 02:06:58 +0100

    API-Gateway: Added verifyToken middleware

---
+++ server/api-gateway/middlewares/verifyToken.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ErrorResponse } from "../utils/ErrorResponse.js";
+
+export const verifyToken = asyncHandler(async (req, res, next) => {
+  const {
+    headers: { authorization },
+  } = req;
+  if (!authorization) {
+    throw new ErrorResponse({
+      message: "Bitte zuerst einloggen.",
+      statusCode: 401,
+      errorType: "Unauthorized",
+      errorCode: "AUTH_005",
+    });
+  }
+  try {
+    const { id } = jwt.verify(authorization, process.env.SECRET_KEY);
+    req.userId = id;
+    next();
+  } catch (err) {
+    throw new ErrorResponse({
+      message: "Ungültiger Token",
+      statusCode: 401,
+      errorType: "Unauthorized",
+      errorCode: "AUTH_006",
+    });
+  }
+});
