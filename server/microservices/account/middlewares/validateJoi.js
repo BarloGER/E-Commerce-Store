52da09e6555ea1a41508f676e8e0dd23de658cd2
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-02-09 02:19:40 +0100

    Microservices/account: Added validateJoi middleware

---
+++ server/microservices/account/middlewares/validateJoi.js
@@ -0,0 +1,26 @@
+import { ErrorResponse } from "../../utils/ErrorResponse.js";
+
+export const validateJoi = (schema) => (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    return next(
+      new ErrorResponse({
+        message: "Keine Daten zum Validieren vorhanden.",
+        statusCode: 400,
+        errorType: "Validation Error",
+        errorCode: "Joi_001",
+      })
+    );
+  }
+
+  const { error } = schema.validate(req.body);
+  return error
+    ? next(
+        new ErrorResponse({
+          message: error.details[0].message,
+          statusCode: 400,
+          errorType: "Validation Error",
+          errorCode: "Joi_002",
+        })
+      )
+    : next();
+};
