0f0cefdbf6f628107cfe0b99f2038606d2910b09
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-02-09 02:39:58 +0100

    Microservices/account: Removed body-parser error and added authRouter

--- server/microservices/account/index.js
+++ server/microservices/account/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
+import { authRouter } from "./routes/authRouter.js";
 
 const app = express();
 const PORT = 8081;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Account Service: GET-Anfrage empfangen");
@@ -16,6 +17,7 @@ app.post("/", (req, res) => {
   res.send("Account Service: POST-Anfrage empfangen");
 });
 
+app.use("/auth", authRouter);
 app.use("*", (req, res) => res.sendStatus(404));
 
 app.listen(PORT, () => {
