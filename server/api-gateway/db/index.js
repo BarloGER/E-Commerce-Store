a81f8166cd3a585cad6e5c82d363ba73e5e3f5d9
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-02-13 11:42:24 +0100

    API-Gateway:  Add environment configuration and update database connection

--- server/api-gateway/db/index.js
+++ server/api-gateway/db/index.js
@@ -1,10 +1,7 @@
 import Pool from "pg-pool";
+import { getDBConnectionConfig } from "../configs/envConfig.js";
 
-const baseConnectionString = process.env.PG_BASE_CONNECTIONSTRING;
+const env = process.env.NODE_ENV;
+const poolConfig = getDBConnectionConfig(env);
 
-export const userDBPool = new Pool({
-  connectionString: `${baseConnectionString}/userDB`,
-  ssl: {
-    rejectUnauthorized: false, // Only for testing
-  },
-});
+export const userDBPool = new Pool(poolConfig);
