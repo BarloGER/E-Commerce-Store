03faa9fa321b711c5046392a0ce26a7098caf20f
Mike Barlozewski <barlozewski.mike@gmail.com>
2024-02-15 16:44:57 +0100

    Microservices/Services: Major change, added Dockerfiles and entry scripts, installed necessary dependencies

---
+++ server/microservices/search/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This script is used as the entry point for the account container. It loads the environment variables from the AWS Parameter Store and then starts the application.
+
+# Function to get an environment variable from the AWS Parameter Store
+get_parameter() {
+  aws ssm get-parameter --name "$1" --with-decryption --query "Parameter.Value" --output text
+}
+
+# Load and export environment variables for the account service
+export SECRET_KEY=$(get_parameter "SECRET_KEY") || { echo "Failed to retrieve SECRET_KEY"; exit 1; }
+export PG_BASE_CONNECTIONSTRING=$(get_parameter "PG_BASE_CONNECTIONSTRING") || { echo "Failed to retrieve PG_BASE_CONNECTIONSTRING"; exit 1; }
+export MONGO_URI=$(get_parameter "MONGO_URI") || { echo "Failed to retrieve MONGO_URI"; exit 1; }
+
+# Now start the application
+exec node index.js
